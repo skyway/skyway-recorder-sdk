ff94e0a496fc843275a694eb20d788f4208a30e8
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-09-17 12:44:39 +0900

    Bundle w/ rollup

--- src/recorder.js
+++ src/recorder.js
@@ -1,6 +1,9 @@
+import { Device } from "mediasoup-client";
+
 export default class Recorder {
   constructor() {
     this._state = "new";
+    this._device = new Device();
   }
 
   async setup(routerCapabilities, transportInfo) {
