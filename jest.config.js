44506564c44ebf5011ba36b1af1f3bd48b023c6d
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-10-21 14:24:51 +0900

    Add restoreMocks

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
+// See https://jestjs.io/docs/en/configuration.html
 module.exports = {
-  testMatch: ["**/__tests__/**/?(*.)test.js"]
+  // test file name should be end with .test.js
+  testMatch: ["**/__tests__/**/?(*.)test.js"],
+  // default is false: we ensure restoring mock states between every test case
+  restoreMocks: true
 };
